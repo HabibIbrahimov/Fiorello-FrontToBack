ed5acbeada5bcae64be89d54bbe998b61a68a45c
HabibIbrahimov <habibyi@code.edu.az>
2022-01-23 14:38:05 +0400

    feat:creat account controller and register form

---
+++ FrontToBAck/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBAck.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
